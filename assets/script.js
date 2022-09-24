a6fe3553f9d52fc2e858c85db4a6c08ee3c2371b
KmtHawk <kmthawk@gmail.com>
2022-09-23 20:43:34 -0400

    added JavaScript code

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for(var x=0; x<topics.length; x++) {console.log(topics[x]);}
+/*
+if (topic === 'HTML') {console.log("Lets's study HTML!");}
+else if (topic === 'CSS') { console.log("Lets study CSS!");}
+else if (topic === 'Git') { console.log("Lets study GIT!");}
+else if (topic === 'JavaScript') { console.log("Lets study JacaScript!");}
+else { console.log('Please try again!');}
+*/
+
