9d34f70fce6e8bc0ccb47eba31cd910dabb672be
KmtHawk <kmthawk@gmail.com>
2022-09-27 20:37:01 -0400

    Added JS code

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,14 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-for(var x=0; x<topics.length; x++) {console.log(topics[x]);}
-/*
-if (topic === 'HTML') {console.log("Lets's study HTML!");}
-else if (topic === 'CSS') { console.log("Lets study CSS!");}
-else if (topic === 'Git') { console.log("Lets study GIT!");}
-else if (topic === 'JavaScript') { console.log("Lets study JacaScript!");}
-else { console.log('Please try again!');}
-*/
-
+var randomTopic = topics [Math.floor(Math.random()*topics.length)];
+function listTopics() {
+    for(var x=0; x<topics.length; x++) {console.log(topics[x]);}}
+function selectTopic() {
+    if (randomTopic=== 'HTML') {console.log("Lets's study HTML!");}
+else if (randomTopic=== 'CSS') { console.log("Lets study CSS!");}
+else if (randomTopic=== 'Git') { console.log("Lets study GIT!");}
+else if (randomTopic=== 'JavaScript') { console.log("Lets study JacaScript!");}
+else { console.log('Please try again!');}}
+console.log('Here are the topics we learned through Prework:');
+listTopics()
+console.log('Which topic should we study first?');
+selectTopic()
